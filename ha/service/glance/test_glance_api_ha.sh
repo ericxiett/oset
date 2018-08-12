444fda30868f4a2b3f35922b3584e8a7a6d518b0
ericxiett <eric_xiett@163.com>
2018-08-12 21:46:30 +0800

    Fix error when check if cirros image existed

--- ha/service/glance/test_glance_api_ha.sh
+++ ha/service/glance/test_glance_api_ha.sh
@@ -19,17 +19,19 @@ prepare_env()
     # NOTE: cirros image MUST be in /root at ctl01 node
     log_info "test_glance_api_ha(prepare_env): Create a new image"
     img_file=/root/cirros-0.3.5-x86_64-disk.img
+    salt "ctl01*" cmd.script salt://ha_test/image_func.sh "create_image $img_file $IMG_NAME"
+    sleep 10
 
-    result=`salt "ctl01*" cmd.script salt://ha_test/image_func.sh "list_images" | grep $IMG_NAME | grep active`
-    if [ -z '$result' ]; then
-        salt "ctl01*" cmd.script salt://ha_test/image_func.sh "create_image $img_file $IMG_NAME"
-        sleep 10
-        result=`salt "ctl01*" cmd.script salt://ha_test/image_func.sh "list_images" | grep $IMG_NAME | grep active`
-        if [ -z '$result' ]; then
-            echo "Failed to create image"
-            exit 1
-        fi
-    fi
+#    result=`salt "ctl01*" cmd.script salt://ha_test/image_func.sh "list_images" | grep $IMG_NAME | grep active`
+#    if [ -z '$result' ]; then
+#        salt "ctl01*" cmd.script salt://ha_test/image_func.sh "create_image $img_file $IMG_NAME"
+#        sleep 10
+#        result=`salt "ctl01*" cmd.script salt://ha_test/image_func.sh "list_images" | grep $IMG_NAME | grep active`
+#        if [ -z '$result' ]; then
+#            echo "Failed to create image"
+#            exit 1
+#        fi
+#    fi
 }
 
 validate()
@@ -51,6 +53,13 @@ validate()
     fi
 }
 
+clean_env()
+{
+    echo "====CLEAN: delete cirros image"
+    log_info "test_glance_api_ha(clean_env): Delete image"
+    salt "ctl01*" cmd.script salt://ha_test/image_func.sh "delete_image $IMG_NAME"
+}
+
 triage()
 {
     # Stop api to test
@@ -65,6 +74,7 @@ triage()
         validate "stop-api" $exp
         if [ $? != 0 ]; then
             result_info "test_ha_glance_api ${RED}FAILED${NC}"
+            clean_env
             return
         fi
     done
@@ -75,18 +85,13 @@ triage()
         validate "start api" "active"
         if [ $? != 0 ]; then
             result_info "test_ha_glance_api ${RED}FAILED${NC}"
+            clean_env
             return
         fi
     done
     result_info "test_ha_glance_api ${GREEN}SUCCESS${NC}"
 }
 
-clean_env()
-{
-    echo "====CLEAN: delete cirros image"
-    log_info "test_glance_api_ha(clean_env): Delete image"
-    salt "ctl01*" cmd.script salt://ha_test/image_func.sh "delete_image $IMG_NAME"
-}
 
 # Execute
 # Default 1 if $1 is null
