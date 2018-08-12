192f3ff863771b33fe8956c668d6098e09549d96
ericxiett <eric_xiett@163.com>
2018-08-12 20:13:21 +0800

    Remove common ref in remote script

--- ha/service/glance/test_glance_api_ha.sh
+++ ha/service/glance/test_glance_api_ha.sh
@@ -17,6 +17,7 @@ prepare_env()
 {
     echo "====PREPARE: update cirros image"
     # NOTE: cirros image MUST be in /root at ctl01 node
+    log_info "test_glance_api_ha(prepare_env): Create a new image"
     img_file=/root/cirros-0.3.5-x86_64-disk.img
     salt "ctl01*" cmd.script salt://ha_test/image_func.sh "create_image $img_file $IMG_NAME"
 
@@ -33,6 +34,7 @@ validate()
     staging=$1
     expected=$2
     # Call image list api to validate
+    log_info "test_glance_api_ha(validate): List images"
     result=`salt "ctl01*" cmd.script salt://ha_test/image_func.sh "list_images" | grep $IMG_NAME`
     if [[ $result =~ $expected ]]; then
         log_info "test_ha_glance_api: $staging validate success!"
@@ -76,6 +78,7 @@ triage()
 clean_env()
 {
     echo "====CLEAN: delete cirros image"
+    log_info "test_glance_api_ha(clean_env): Delete image"
     salt "ctl01*" cmd.script salt://ha_test/image_func.sh "delete_image $IMG_NAME"
 }
 
--- ha/utils/image_func.sh
+++ ha/utils/image_func.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-. ./common.sh
-. ./openrc.sh
-
 set -ex
 
 DATES=$(date "+%Y-%m-%d")
+
+openrc()
+{
+    export OS_IDENTITY_API_VERSION=3
+    export OS_AUTH_URL=http://10.200.0.20:35357/v3
+    export OS_PROJECT_DOMAIN_NAME=Default
+    export OS_USER_DOMAIN_NAME=Default
+    export OS_PROJECT_NAME=admin
+    export OS_TENANT_NAME=admin
+    export OS_USERNAME=admin
+    export OS_PASSWORD=vjYXTG6vptMNweIx
+    export OS_REGION_NAME=RegionOne
+    export OS_INTERFACE=internal
+    export OS_ENDPOINT_TYPE="internal"
+    export OS_CACERT="/etc/ssl/certs/ca-certificates.crt"
+}
 
 # Function list
 # create_image(): Create a new image
@@ -14,21 +27,18 @@ DATES=$(date "+%Y-%m-%d")
 
 create_image()
 {
-    log_info "Create a new image"
     openrc
     openstack image create --file $1 --disk-format qcow2 --container-format bare $2 | tee -a /tmp/hatest_detailed.log
 }
 
 list_images()
 {
-    log_info "List images"
     openrc
     openstack image list | tee -a /tmp/hatest_detailed.log
 }
 
 delete_image()
 {
-    log_info "Delete image"
     openrc
     openstack image delete $1 | tee -a /tmp/hatest_detailed.log
 }
